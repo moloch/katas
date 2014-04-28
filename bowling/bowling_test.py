589d023cd20c70f907b7512ce55a523f79c096ba
moloch <dario.coco@gmail.com>
2014-04-28 14:28:05 +0200

    Test spare [r]

--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -12,6 +12,12 @@ class BowlingGameTest(unittest.TestCase):
   def testAllOnes(self):
     self.__rollMany(1,20)
 
+  def testSpare(self):
+    self.g.roll(4)
+    self.g.roll(6)
+    self.g.roll(4)
+    self.assertEqual(14+4, self.g.get_score())
+
   def __rollMany(self, pins, expected_score):
     for i in range(20):
       self.g.roll(pins)
