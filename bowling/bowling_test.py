565e357bb69e057185a8f5c690e1864fbf6f5188
moloch <dario.coco@gmail.com>
2014-04-28 10:04:59 +0200

    The second test (all ones) test refactoring part 1 [g]

--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -3,17 +3,18 @@ from bowling import Game
 
 class BowlingGameTest(unittest.TestCase):
 
+  def setUp(self):
+    self.g = Game()
+
   def testGutterGame(self):
-    g = Game()
     for i in range(20):
-      g.roll(0)
-    self.assertEqual(0, g.score())
+      self.g.roll(0)
+    self.assertEqual(0, self.g.score())
   
   def testAllOnes(self):
-    g = Game()
     for i in range(20):
-      g.roll(1)
-    self.assertEqual(20, g.score())  
+      self.g.roll(1)
+    self.assertEqual(20, self.g.score())  
 
 if __name__ == '__main__':
     unittest.main()
