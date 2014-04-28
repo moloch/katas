d1c3eafe27ec129530294da6a98a0f61401f0c3f
moloch <dario.coco@gmail.com>
2014-04-28 09:39:50 +0200

    The second test (all ones) [r]

--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -7,7 +7,13 @@ class BowlingGameTest(unittest.TestCase):
     g = Game()
     for i in range(20):
       g.roll(0)
-    self.assertEqual(0, g.score())  
+    self.assertEqual(0, g.score())
+  
+  def testAllOnes(self):
+    g = Game()
+    for i in range(20):
+      g.roll(1)
+    self.assertEqual(20, g.score())  
 
 if __name__ == '__main__':
     unittest.main()
