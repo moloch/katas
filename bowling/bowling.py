a1df5e58ae0d1b8a3c7e2c797c4d0a65fe4e94c6
moloch <dario.coco@gmail.com>
2014-04-29 09:32:08 +0200

    refactoring roll and get_score methods

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -2,9 +2,12 @@ class Game:
 
   def __init__(self):
     self.score = 0
+    self.rolls = []
 
   def roll(self, pins):
-    self.score += pins
+    self.rolls.append(pins)
 
   def get_score(self):
+    for roll in self.rolls:
+      self.score += roll
     return self.score
