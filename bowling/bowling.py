507cd3186691a8a28c2818e1acf4f8a5a2d3257e
moloch <dario.coco@gmail.com>
2014-04-30 11:34:23 +0200

    Rolls logic changed to use Frames [g]

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -10,28 +10,36 @@ class Frame:
     else:
       return False
 
+  def get_rolls(self):
+    return self.rolls
+
   def get_points(self):
     return sum(self.rolls)
 
   def is_gutter(self):
-    return sum(self.rolls) == 10 and len(self.rolls) == 2
+    return self.is_full() and len(self.rolls) == 2
 
   def is_strike(self):
-    return sum(self.rolls) == 10 and len(self.rolls) == 1
+    return self.is_full() and len(self.rolls) == 1
+
+  def is_full(self):
+    return sum(self.rolls) == 10
 
 class Game:
 
   def __init__(self):
     self.score = 0
-    self.rolls = []
     self.frames = []
 
   def roll(self, pins):
-    self.rolls.append(pins)
-
+    if len(self.frames) == 0 or len(self.frames[-1].get_rolls()) > 2:
+      frame = Frame()
+      self.add_frame(frame)
+    self.frames[-1].roll(pins) 
+ 
   def get_score(self):
-    for roll in self.rolls:
-      self.score += roll
+    for frame in self.frames:
+      self.score += frame.get_points()
     return self.score
 
   def add_frame(self, frame):
