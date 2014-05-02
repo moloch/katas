ae8eee68a484a03771be74ca83ddf7555b1aa270
moloch <dario.coco@gmail.com>
2014-05-02 14:16:10 +0200

    Adding two numbers [r]

--- string_calculator/string_calculator_test.py
+++ string_calculator/string_calculator_test.py
@@ -7,7 +7,13 @@ class CalculatorTest(unittest.TestCase):
     self.calculator = Calculator()
 
   def testAddEmptyString(self):
-    self.assertEqual(0, self.calculator.add(""));
+    self.assertEqual(0, self.calculator.add(""))
+
+  def testAddOne(self):
+    self.assertEqual(1, self.calculator.add("1"))
+
+  def testAddTwoNumbers(self):
+    self.assertEqual(5, self.calculator.add("3,2"))
 
 if __name__ == "__main__":
   unittest.main()
