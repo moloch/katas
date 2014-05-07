5dad08db71bd61a21b731e7be04e3a418b6e468a
moloch <dario.coco@gmail.com>
2014-05-07 14:34:50 +0200

    Convert 2 from decimal to roman [r]

--- roman_numerals/roman_numerals_test.py
+++ roman_numerals/roman_numerals_test.py
@@ -6,5 +6,8 @@ class RomanNumeralsTest(unittest.TestCase):
   def testConvertOneFromDecimalToRoman(self):
     self.assertEqual("I", RomanNumber(1).get_roman_value())
 
+  def testConvertTwoFromDecimalToRoman(self):
+    self.assertEqual("II", RomanNumber(2).get_roman_value())
+
 if __name__ == '__main__':
   unittest.main()
