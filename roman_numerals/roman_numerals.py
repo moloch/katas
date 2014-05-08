0ce39061f1eda85993c5ceef0db8ceeaed6d35e3
moloch <dario.coco@gmail.com>
2014-05-08 10:40:14 +0200

    Refactoring, test IV [r]

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,10 +1,18 @@
+from collections import OrderedDict
+
 class RomanNumber():
 
   def __init__(self, value):
     self.value = value
+    self.numerals = [(1,'I'),(5,'V')]
 
   def get_roman_value(self):
     result = ""
-    for i in range(self.value):
-      result += "I"
+    current_value = self.value
+    while len(self.numerals) > 0:
+      numeral = self.numerals.pop()
+      print numeral[0], numeral[1]
+      while numeral[0] <= current_value:
+        result += numeral[1]
+        current_value -= numeral[0] 
     return result 
