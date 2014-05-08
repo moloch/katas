245abcb732567f89b1bbedf0ccb9a6a95bbc215b
moloch <dario.coco@gmail.com>
2014-05-08 10:49:36 +0200

    Added tests from 6 to 8. 4 Still in red [r]

--- roman_numerals/roman_numerals_test.py
+++ roman_numerals/roman_numerals_test.py
@@ -4,16 +4,28 @@ from roman_numerals import RomanNumber
 class RomanNumeralsTest(unittest.TestCase):
   
   def testConvertOneFromDecimalToRoman(self):
-    self.assertEqual("I", RomanNumber(1).get_roman_value())
+    self.__decimalToRoman("I", 1)
 
   def testConvertTwoFromDecimalToRoman(self):
-    self.assertEqual("II", RomanNumber(2).get_roman_value())
+    self.__decimalToRoman("II", 2)
 
   def testConvertThreeFromDecimalToRoman(self):
-    self.assertEqual("III", RomanNumber(3).get_roman_value())
+    self.__decimalToRoman("III", 3)
 
   def testConvertFourFromDecimalToRoman(self):
-    self.assertEqual("IV", RomanNumber(4).get_roman_value())
+    self.__decimalToRoman("IV", 4)
+
+  def testConvertFiveFromDecimalToRoman(self):
+    self.__decimalToRoman("V", 5)
+
+  def testConvertFromSixToEightFromDecimalToRoman(self):
+    self.__decimalToRoman("VI", 6)
+    self.__decimalToRoman("VII", 7)
+    self.__decimalToRoman("VIII", 8)
+
+  def __decimalToRoman(self, roman, decimal):
+    self.assertEqual(roman, RomanNumber(decimal).get_roman_value())
+    
 
 if __name__ == '__main__':
   unittest.main()
