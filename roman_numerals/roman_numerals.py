597edeb15fd0942f0a5661690ccedacb38dab36d
moloch <dario.coco@gmail.com>
2014-05-08 17:36:18 +0200

    Added test for 9 [r]

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -4,20 +4,22 @@ class RomanNumber():
 
   def __init__(self, value):
     self.value = value
-    self.numerals = [(1,'I'),(5,'V')]
+    self.numerals = [(10, 'X'),(5, 'V'),(1,'I')]
 
   def get_roman_value(self):
     result = ""
     current_value = self.value
-    while len(self.numerals) > 0:
-      numeral = self.numerals.pop()
+    for i, numeral in enumerate(self.numerals):
       consecutive_numerals = 0
       while numeral[0] <= current_value:
         if consecutive_numerals < 3:
           result += numeral[1]
           consecutive_numerals += 1
         else:
-          result = result.replace('III', 'IV')
+          previous = result[:-3] + numeral[1]*3
+          actual = numeral[1] + self.numerals[i-1][1]
+          print previous, actual
+          result = result.replace(previous, actual)
           consecutive_numerals = 0
         current_value -= numeral[0]
     return result 
