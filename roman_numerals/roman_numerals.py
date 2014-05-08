4e3163e881d15954d39bce4b492843d512b2fb99
moloch <dario.coco@gmail.com>
2014-05-08 18:18:35 +0200

    Added test for 9 [r]

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -4,12 +4,12 @@ class RomanNumber():
 
   def __init__(self, value):
     self.value = value
-    self.numerals = [(10, 'X'),(5, 'V'),(1,'I')]
+    self.numerals = OrderedDict([(10, 'X'),(5, 'V'),(1,'I')])
 
   def get_roman_value(self):
     result = ""
     current_value = self.value
-    for i, numeral in enumerate(self.numerals):
+    for i, numeral in enumerate(self.numerals.iteritems()):
       consecutive_numerals = 0
       while numeral[0] <= current_value:
         if consecutive_numerals < 3:
@@ -17,8 +17,8 @@ class RomanNumber():
           consecutive_numerals += 1
         else:
           previous = result[:-3] + numeral[1]*3
-          actual = numeral[1] + self.numerals[i-1][1]
-          print previous, actual
+          actual = numeral[1] + self.numerals.items()[i-1][1]
+          print previous, actual #Trovare l'elemento precedente al primo nel dict self.numerals
           result = result.replace(previous, actual)
           consecutive_numerals = 0
         current_value -= numeral[0]
