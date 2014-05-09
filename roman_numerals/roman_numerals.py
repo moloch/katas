fcff00d79e0cd25b40191ce2072b537290a3419e
moloch <dario.coco@gmail.com>
2014-05-09 13:58:29 +0200

    Numbers until 29 [g]

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -5,7 +5,7 @@ class RomanNumber():
   def __init__(self, value):
     self.value = value
     self.numerals = OrderedDict([(10, 'X'),(5, 'V'),(1,'I')])
-    self.numbers = {v:k for k, v in self.numerals.items()}
+    self.numbers = OrderedDict([(v,k) for k, v in self.numerals.items()])
 
   def get_roman_value(self):
     result = ""
@@ -18,7 +18,7 @@ class RomanNumber():
           result += numeral[1]
           consecutive_numerals += 1
         else:
-          previous = result[-5:]
+          previous = result[-4:]
           if self.numbers[previous[0]] % 10 != 0:
             actual = previous[1] + numerals[numerals.index(previous[0])-1]
           else:
