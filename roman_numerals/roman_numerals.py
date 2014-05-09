272b249d0d399c330e94d458c89dd4e0388a62c5
moloch <dario.coco@gmail.com>
2014-05-09 15:02:30 +0200

    A roman digit should not be repeated more than 3 times if it's I X C M. V L D can never be repeated. Some test with repetitions of I and X is failing

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -11,20 +11,9 @@ class RomanNumber():
     result = ""
     current_value = self.value
     for decimal_val in self.numerals:
-      consecutive_digits = 0
       roman_val = self.numerals[decimal_val]
       while decimal_val <= current_value:
-        if consecutive_digits < 3:
-          result += roman_val
-          consecutive_digits += 1
-        else:
-          previous = result[-4:]
-          if previous[0] in ['L','V','I']:
-            actual = previous[1] + self.get_prev_numeral(previous[0])
-          else:
-            actual = previous[0] + previous[2] + self.get_prev_numeral(previous[1])
-          result = result.replace(previous, actual)
-          consecutive_digits = 0
+        result += roman_val
         current_value -= decimal_val
     return result
 
