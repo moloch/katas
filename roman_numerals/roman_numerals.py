dcda4c02378bc6d70f0c3287da4eb705df053746
moloch <dario.coco@gmail.com>
2014-05-09 11:51:25 +0200

    Convert  from decimal to roman until 18 [g]

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -5,6 +5,7 @@ class RomanNumber():
   def __init__(self, value):
     self.value = value
     self.numerals = OrderedDict([(10, 'X'),(5, 'V'),(1,'I')])
+    self.numbers = {v:k for k, v in self.numerals.items()}
 
   def get_roman_value(self):
     result = ""
@@ -17,7 +18,10 @@ class RomanNumber():
           consecutive_numerals += 1
         else:
           previous = result[:-3] + numeral[1]*3
-          actual = previous[1] + self.numerals.values()[self.numerals.values().index(previous[0])-1]
+          if self.numbers[previous[0]] % 10 != 0:
+            actual = previous[1] + self.numerals.values()[self.numerals.values().index(previous[0])-1]
+          else:
+            actual = previous[0] + previous[1] + self.numerals.values()[self.numerals.values().index(previous[1])-1]
           result = result.replace(previous, actual)
           consecutive_numerals = 0
         current_value -= numeral[0]
--- roman_numerals/roman_numerals_test.py
+++ roman_numerals/roman_numerals_test.py
@@ -26,6 +26,17 @@ class RomanNumeralsTest(unittest.TestCase):
   def testConvertNineFromDecimalToRoman(self):
     self.__decimalToRoman("IX", 9)
 
+  def testConvertFromTenToEighteenFromDecimalToRoman(self):
+    self.__decimalToRoman("X", 10)
+    self.__decimalToRoman("XI", 11)
+    self.__decimalToRoman("XII", 12)
+    self.__decimalToRoman("XIII", 13)
+    self.__decimalToRoman("XIV", 14)
+    self.__decimalToRoman("XV", 15)
+    self.__decimalToRoman("XVI", 16)
+    self.__decimalToRoman("XVII", 17)
+    self.__decimalToRoman("XVIII", 18)
+
   def __decimalToRoman(self, roman, decimal):
     self.assertEqual(roman, RomanNumber(decimal).get_roman_value())
     
