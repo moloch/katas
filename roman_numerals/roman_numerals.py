be03d837ebef0aabd53ec5168d7c42a946f46fb5
moloch <dario.coco@gmail.com>
2014-05-09 14:22:40 +0200

    Refactoring

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -10,20 +10,25 @@ class RomanNumber():
   def get_roman_value(self):
     result = ""
     current_value = self.value
-    for i, numeral in enumerate(self.numerals.iteritems()):
+    for decimal_val in self.numerals:
       consecutive_numerals = 0
       numerals = self.numerals.values()
-      while numeral[0] <= current_value:
+      roman_val = self.numerals[decimal_val]
+      while decimal_val <= current_value:
         if consecutive_numerals < 3:
-          result += numeral[1]
+          result += roman_val
           consecutive_numerals += 1
         else:
           previous = result[-4:]
           if self.numbers[previous[0]] % 10 != 0:
-            actual = previous[1] + numerals[numerals.index(previous[0])-1]
+            actual = previous[1] + self.get_prev_numeral(previous[0])
           else:
-            actual = previous[0] + previous[2] + numerals[numerals.index(previous[1])-1]
+            actual = previous[0] + previous[2] + self.get_prev_numeral(previous[1])
           result = result.replace(previous, actual)
           consecutive_numerals = 0
-        current_value -= numeral[0]
-    return result 
+        current_value -= decimal_val
+    return result
+
+  def get_prev_numeral(self, n):
+    numerals = self.numerals.values()
+    return numerals[numerals.index(n)-1] 
